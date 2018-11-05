008d35394de32d748a789b1a19dd5002b95a236d
Adam Pash <adam.pash@gmail.com>
2018-11-05 15:01:47 -0800

    add env var

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import {
   Loading
 } from "./styles";
 
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.API_URL || "http://localhost:3000";
 const ADS_URL =
   "https://www.facebook.com/ads/archive/?active_status=all&ad_type=political_and_issue_ads&country=US&page_ids[0]=PAGE_ID&q=QUERY_STR";
 
