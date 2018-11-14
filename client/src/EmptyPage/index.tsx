a6293e7813f255fb05ba8150f68d5a8ab54a695e
Adam Pash <adam.pash@gmail.com>
2018-11-14 11:05:27 -0800

    add *language

--- client/src/EmptyPage/index.tsx
+++ client/src/EmptyPage/index.tsx
@@ -6,17 +6,35 @@ interface Props {}
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   font-size: 16px;
   i {
     margin: 0 16px;
   }
+  div {
+    margin-top: 3px;
+  }
+`;
+
+const Asterisk = styled.div`
+  font-size: 14px;
+  padding: 16px 0;
+  &:before {
+    content: "*";
+    color: red;
+  }
 `;
 
 const EmptyPage = (props: Props) => (
-  <Container>
-    <Icon type={IconType.ArrowBack} /> Click on a page to view more details
-  </Container>
+  <>
+    <Container>
+      <Icon type={IconType.ArrowBack} />{" "}
+      <div>
+        Click on a page to view more details
+        <Asterisk>Indicates page name cannot be found in FEC data</Asterisk>
+      </div>
+    </Container>
+  </>
 );
 
 export default EmptyPage;
